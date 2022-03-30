952fb29f132d1ba26029be2acb30d5c8366840d5
Zoe McCarthy <zoemccarthy12@gmail.com>
2022-03-29 20:41:57 -0400

    added devenv to config and repo

--- startup.jl
+++ startup.jl
@@ -1,7 +1,7 @@
 using Pkg
 
 # Environment Stack knowledge from https://docs.julialang.org/en/v1/manual/code-loading/#Environment-stacks
-DEVENV_LOC = joinpath(homedir(), ".julia", "devenv")
+DEVENV_LOC = joinpath(homedir(), ".julia", "config", "devenv")
 if !(DEVENV_LOC in LOAD_PATH)
     @info "Adding $DEVENV_LOC to LOAD_PATH"
     push!(LOAD_PATH, DEVENV_LOC)
